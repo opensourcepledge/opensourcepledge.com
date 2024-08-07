d136e2e6f1b24d435661c36179c3562cb8ff32e1
Vlad-Stefan Harbuz <vlad@vladh.net>
2024-08-07 19:37:39 +0100

    Improve leaderboard design

--- src/member_util.ts
+++ src/member_util.ts
@@ -4,24 +4,31 @@ import exampleMember from '../contrib/example-schema.json';
 
 const isDev = import.meta.env.DEV;
 
+/**
+ * Generates some member data for testing to be used in dev mode.
+ */
+function getMockMembers() {
+  const mockDevCounts = [
+    1, 34, 79, 230, 1004, 4052, 10245, 11245, 17353, 21324, 50124,
+  ]
+  return mockDevCounts.map((mockDevCount, idx) => {
+    let newMember = structuredClone(exampleMember);
+    newMember.name = `Example Member (${mockDevCount} ${mockDevCount == 1 ? "Dev" : "Devs"})`;
+    newMember.urlSquareLogoWithBackground = `example-member-${idx}.svg`;
+    newMember.annualReports = newMember.annualReports.map((report) => {
+      report.averageNumberOfDevs = mockDevCount;
+      return report;
+    });
+    return {
+      id: `example-member-${idx}`,
+      data: newMember,
+    };
+  });
+}
+
 export async function getMembers() {
   if (isDev) {
-    // In development mode, generate some mock data for testing.
-    const mockDevCounts = [
-      1, 34, 79, 230, 1004, 4052, 10245, 11245, 17353, 21324, 50124,
-    ]
-    return mockDevCounts.map((mockDevCount) => {
-      let newMember = structuredClone(exampleMember);
-      newMember.name = `Sentry ${mockDevCount}`;
-      newMember.annualReports = newMember.annualReports.map((report) => {
-        report.averageNumberOfDevs = mockDevCount;
-        return report;
-      });
-      return {
-        id: `sentry-${mockDevCount}`,
-        data: newMember,
-      };
-    });
+    return getMockMembers();
   } else {
     return getCollection('members');
   }
