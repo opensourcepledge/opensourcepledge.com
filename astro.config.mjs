86b209aec0540bc43a7f38023a3f91d670a8e2f1
Vlad-Stefan Harbuz <vlad@vladh.net>
2024-08-30 10:40:11 +0100

    Remove Tailwind
    Tailwind is nice for prototyping. However, we'll now start to write a
    lot more custom CSS, and as we do that, our custom CSS will interact
    with Tailwind's CSS, and it will be difficult to tell which styles are
    coming from where, which will add work. This is a good time to remove
    Tailwind, since we're redoing most of the CSS anyway.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://osspledge.com",
-  integrations: [tailwind(), sentry()],
+  integrations: [sentry()],
 });
