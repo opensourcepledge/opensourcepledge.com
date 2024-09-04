1def49cff14bb7d132f590f232d38d1c82a3ea4c
Vlad-Stefan Harbuz <vlad@vladh.net>
2024-09-04 20:32:16 +0100

    Remove Sentry SDK

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import sentry from "@sentry/astro";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://osspledge.com",
-  integrations: [sentry()],
 });
