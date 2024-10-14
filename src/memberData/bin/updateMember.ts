51b08cdb15aeb02d4ab8f049ab236b5d68d7b00e
Vlad-Stefan Harbuz <vlad@vladh.net>
2024-10-14 15:40:45 +0100

    Add comprehensive JSON file monitoring
    Closes #113.

---
+++ src/memberData/bin/updateMember.ts
@@ -0,0 +1,68 @@
+#!/usr/bin/env -S npx tsx
+
+// © 2024 Vlad-Stefan Harbuz <vlad@vladh.net>
+// SPDX-License-Identifier: Apache-2.0
+
+import fs from "fs";
+import fetch from "node-fetch";
+import { Octokit } from "@octokit/rest";
+
+import { makeIssueIfNotExists, isReportOverdue, isReportDueSoon } from "../common.ts";
+import { MemberException } from "../common.ts";
+
+
+const USAGE = `USAGE: ./bin/update-member foocorp https://foocorp.example.com/foocorp.json
+Must be run in the repo's root.`;
+
+
+async function main() {
+  const args = process.argv.slice(2);
+  if (args.length != 2) {
+    console.error(USAGE);
+    process.exit(1);
+  }
+  const slug = args[0];
+  const url = args[1];
+
+  if (!('GITHUB_TOKEN' in process.env)) {
+    console.error('Please define the GITHUB_TOKEN environment variable with your personal access token.');
+    process.exit(1);
+  }
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+  const localPath = `./src/content/members/${slug}.json`;
+  console.log(`Fetching member: ${slug}`);
+  console.log(`\t* Remote URL: ${url}`);
+  console.log(`\t* Local path: ${localPath}`);
+
+  if (fs.existsSync(localPath + '.lock')) {
+    return;
+  }
+
+  let remoteMemberData = undefined;
+  try {
+    remoteMemberData = await fetch(url)
+      .then((res) => res.text())
+      .then((text) => JSON.parse(text));
+  } catch (e) {
+    await makeIssueIfNotExists(octokit, MemberException.JsonFileNotAccessible, slug, url, undefined);
+    return;
+  }
+
+  if (isReportOverdue(remoteMemberData)) {
+    await makeIssueIfNotExists(octokit, MemberException.ReportOverdue, slug, url, remoteMemberData);
+  } else if (isReportDueSoon(remoteMemberData)) {
+    await makeIssueIfNotExists(octokit, MemberException.ReportDueSoon, slug, url, remoteMemberData);
+  }
+
+  try {
+    fs.writeFileSync(localPath, JSON.stringify(remoteMemberData, null, 2));
+  } catch (e) {
+    console.error(`ERROR: Failed to write to ${localPath}`, e);
+    process.exit(1);
+  }
+}
+
+
+main()
--- src/memberFormatting.ts
+++ src/memberFormatting.ts
@@ -1,15 +1,16 @@
 // © 2024 Vlad-Stefan Harbuz <vlad@vlad.website>
 // SPDX-License-Identifier: Apache-2.0
 
-import { getCollection } from 'astro:content';
-import type { MemberWithId, MemberReport } from "./content/config.ts";
 import memberRoles from "./memberRoles.json"
 import type { Map } from './util.ts';
+import type { MemberWithId, MemberReport } from "./content/config.ts";
+import { getCollection } from 'astro:content';
+import { sortReportsForMemberWithId } from "./memberData/common.ts";
 
 export async function getMembers(): Promise<MemberWithId[]> {
   return (await getCollection('members'))
     .filter((member) => member.id in (memberRoles as Map))
-    .map(sortReportsForMember);
+    .map(sortReportsForMemberWithId);
 }
 
 export function getDollarsPerDev(report: MemberReport) {
@@ -109,19 +110,3 @@ export function sortMembersByDollarsPerDev(members: MemberWithId[]): MemberWithI
   });
 }
 
-function sortReportsForMember(member: MemberWithId): MemberWithId {
-  const sortedReports = member.data.annualReports.toSorted((a, b) =>
-    new Date(a.dateYearEnding) < new Date(b.dateYearEnding) ? 1 : -1
-  );
-
-  return {
-    ...member,
-    data: {
-      ...member.data,
-      annualReports: sortedReports as [
-        MemberReport,
-        ...MemberReport[],
-      ],
-    },
-  };
-}
