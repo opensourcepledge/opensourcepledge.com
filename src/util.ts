f75c891a8f3f349b6bef05702fe05bfb51e165c4
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-10-31 18:11:58 +0000

    TableOfContents: simplify, refactor to run server-side

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,4 @@
 // © 2024 Vlad-Stefan Harbuz <vlad@vlad.website>
-// © 2024 Safwan Yaseen Parkar <safwanparkar6@gmail.com>
 // SPDX-License-Identifier: Apache-2.0
 
 export interface Map {
@@ -15,28 +14,4 @@ export function shuffle(arr: any[]) {
 
 export function range(n: number) {
   return [...Array(n).keys()];
-}
-
-export function extractSections() {
-  const sections = document.querySelectorAll("section[id]");
-  return Array.from(sections).map((section) => ({
-    id: section.id,
-    title: section.querySelector("h2")?.textContent || section.id,
-  }));
-}
-
-// this script generates the Table of Contents elements from the sections in the page
-// it should be included in the page after the sections have been defined
-export function generateTableOfContentItems() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const sections = extractSections();
-    const tocElement = document.querySelector("#table-of-contents ol");
-    if (tocElement) {
-      tocElement.innerHTML = sections
-        .map(
-          (section) => `<li><a href="#${section.id}">${section.title}</a></li>`
-        )
-        .join("");
-    }
-  });
 }
