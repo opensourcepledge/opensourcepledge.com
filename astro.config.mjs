cf4299dd9a511e7126b0320587b5d8dc7b3b79d4
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-12-10 10:25:06 +0000

    Add Sanity configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
 import { defineConfig } from "astro/config";
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://opensourcepledge.com",
+
   redirects: {
     'opensourcepledge.pdf': 'open-source-pledge-one-pager.pdf',
     'osspledge.pdf': 'open-source-pledge-one-pager.pdf',
   },
+
+  integrations: [
+    sanity({
+      projectId: '4jfayhvz',
+      dataset: 'production',
+      useCdn: false, // Because we're building statically
+      studioBasePath: '/studio',
+    }),
+    react()
+  ],
 });
