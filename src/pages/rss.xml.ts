0d949cb719280dd6db0a33be2d1e3ebca59e16c8
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-12-20 15:36:32 +0000

    Blog: add RSS feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,47 @@
+import type { APIRoute } from 'astro';
+import { getRssString } from '@astrojs/rss';
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
+
+import type { SanityDocument } from "@sanity/client";
+import { sanityClient } from "sanity:client";
+
+const ARTICLES_QUERY = `*[_type == "article"]|order(publishDateTime desc){
+  _id,
+  title,
+  slug,
+  publishDateTime,
+  author->{name, slug, avatar}
+}`;
+
+export const GET: APIRoute = async (context) => {
+  const articles = (await sanityClient.fetch<SanityDocument[]>(ARTICLES_QUERY))
+    .filter((a) => dayjs(a.publishDateTime).isBefore(dayjs().utc()));
+
+  const rssString = await getRssString({
+    title: 'The Open Source Pledge Blog',
+    description: 'Exploring ways to improve how the Open Source ecosystem is funded.',
+    site: context.site as URL,
+    items: articles.map((a) => {
+      return {
+        title: a.title,
+        pubDate: a.publishDateTime,
+        author: a.author.name,
+        link: `/blog/${a.slug.current}/`,
+      };
+    }),
+    stylesheet: '/rss.xsl',
+  });
+  const res = new Response(rssString, {
+    headers: {
+      // `application/rss+xml` is technically the correct content type, but it
+      // results in the XML file being downloaded instead of displayed in eg
+      // Firefox, which defeats the purpose of our styling; and `text/xml` is
+      // the most widely used content type for RSS.
+      'Content-Type': 'text/xml; charset=utf-8',
+    },
+  });
+  return res;
+}
