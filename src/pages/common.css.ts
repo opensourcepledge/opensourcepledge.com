3cc902fadc971022b8b67aeee6b32a225c593a5c
Vlad-Stefan Harbuz <vlad@vlad.website>
2024-12-20 15:36:32 +0000

    Blog: add RSS feed

---
+++ src/pages/common.css.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+
+import css from '../styles/common.css?raw';
+
+export const GET: APIRoute = async (_context) => {
+  // NOTE: This is a bit of a hack, but I haven't found a straightforward way
+  // to have Astro build a separate CSS file in production. Know how? Send a
+  // PR!
+  // NOTE: At the time of writing, this is used only in `rss.xsl`. It doesn't
+  // seem straightforwardly possible to embed the CSS in the XSL file.
+  return new Response(css);
+}
