4089bc03cb5e90e4bd3d4b300bf1bc7e414e648d
Vlad-Stefan Harbuz <vlad@vlad.website>
2025-01-29 11:29:11 +0100

    Add highlightBox style to block

--- sanity.config.ts
+++ sanity.config.ts
@@ -119,7 +119,20 @@ export default defineConfig({
             description: "The article's content",
             type: 'array',
             of: [
-              { type: 'block' },
+              {
+                type: 'block',
+                styles: [
+                  {title: 'Normal', value: 'normal'},
+                  {title: 'H1', value: 'h1'},
+                  {title: 'H2', value: 'h2'},
+                  {title: 'H3', value: 'h3'},
+                  {title: 'H4', value: 'h4'},
+                  {title: 'H5', value: 'h5'},
+                  {title: 'H6', value: 'h6'},
+                  {title: 'Quote', value: 'blockquote'},
+                  {title: 'Highlight Box', value: 'highlightBox'},
+                ],
+              },
               {
                 type: 'image',
                 title: 'Captioned Image',
