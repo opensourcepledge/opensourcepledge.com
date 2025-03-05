ee4e69327b374d538a593dc4da0fa3a381a88819
Vlad-Stefan Harbuz <vlad@vlad.website>
2025-03-05 22:28:37 +0000

    members: add jobsUrl to schema

--- src/schemas/members.ts
+++ src/schemas/members.ts
@@ -17,6 +17,7 @@ export const memberSchema = z.object({
   description: z.string(),
   shortDescription: z.string(),
   url: z.string().url(),
+  jobsUrl: z.string().url().optional(),
   joinDate: z.string().date(),
   annualReports: memberReportSchema.array().nonempty(),
 });
