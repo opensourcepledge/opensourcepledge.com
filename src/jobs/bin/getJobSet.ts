7610be29acc96c686035b7a2e3ea453622c93054
Vlad-Stefan Harbuz <vlad@vlad.website>
2025-03-18 16:44:22 +0000

    Jobs: improve caching procedure

--- src/jobs/bin/getJobSet.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S npx tsx
-
-// © 2025 Functional Software, Inc. dba Sentry
-// SPDX-License-Identifier: Apache-2.0
-
-import { getJobSet } from '../index.ts';
-
-console.log(JSON.stringify(await getJobSet()));
