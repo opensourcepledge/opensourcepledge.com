984538493be3c558e12f98f6551cdf3152168067
Gus Libens <27970303+goestav@users.noreply.github.com>
2025-06-19 20:38:47 +0200

    fix: include the article content for RSS feed items
    Full text in an RSS feed is more convenient for users, see [1] for more information.
    
    [1]: https://openrss.org/blog/full-text-in-rss-please

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,11 +10,14 @@ dayjs.extend(utc);
 
 import type { SanityDocument } from "@sanity/client";
 import { sanityClient } from "sanity:client";
+import { toHTML } from '@portabletext/to-html'
+import { urlForImage } from '../sanityUtil.ts';
 
 const ARTICLES_QUERY = `*[_type == "article"]|order(publishDateTime desc){
   _id,
   title,
   slug,
+  content,
   publishDateTime,
   author->{name, slug, avatar}
 }`;
@@ -33,6 +36,48 @@ export const GET: APIRoute = async (context) => {
         pubDate: a.publishDateTime,
         author: a.author.name,
         link: `/blog/${a.slug.current}/`,
+        description: toHTML(a.content, {
+          components: {
+            types: {
+							captionedImage: ({ value }) => {
+                const styleAttribute = value.maxWidth && `style="max-width: ${value.maxWidth}"`;
+                const captionHTML = value.url
+                  ? `<a href="${value.url}">${value.caption ?? ''}</a>`
+                  : `<span>${value.caption ?? ''}</span>`
+
+								return `
+                  <figure>
+                    <img
+                      src="${urlForImage(value.asset)}"
+                      title="${value.titleText ?? ''}"
+                      alt="${value.altText ?? ''}"
+                      ${styleAttribute ?? ''}
+                    >
+                    <figcaption><em>${captionHTML}</em></figcaption>
+                  </figure>
+                `;
+							},
+							youtubeEmbed: ({ value }) => `
+                <iframe
+                  width="980"
+                  height="551"
+                  src="https://www.youtube-nocookie.com/embed/${value.videoId}"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+              `,
+							horizontalRule: () => "<hr />",
+							callToAction: ({ value }) => `
+                <section style="text-align: center; margin-top: 4rem">
+                  <h2>${value.text}</h2>
+                  <a href="/join" class="margin-top: 1rem; text-transform: uppercase;">Join the Pledge</a>
+                </section>
+              `,
+					  },
+          }
+        })
       };
     }),
     stylesheet: '/rss.xsl',
