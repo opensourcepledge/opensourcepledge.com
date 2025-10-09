fc0f378ae7a2797a5b903a9ea4e3b10ad97a2ded
Vlad-Stefan Harbuz <vlad@vlad.website>
2025-10-09 19:05:52 +0200

    correct redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,10 @@ export default defineConfig({
   site: "https://opensourcepledge.com",
 
   redirects: {
-    'images/nasdaq-2024.jpg': 'images/nasdaq-2024-10.jpg',
-    'images/nasdaq-2024@1000px.jpg': 'images/nasdaq-2024-10@1000px.jpg',
-    'opensourcepledge.pdf': 'open-source-pledge-one-pager.pdf',
-    'osspledge.pdf': 'open-source-pledge-one-pager.pdf',
+    'images/nasdaq-2024.jpg': '/images/nasdaq-2024-10.jpg',
+    'images/nasdaq-2024@1000px.jpg': '/images/nasdaq-2024-10@1000px.jpg',
+    'opensourcepledge.pdf': '/open-source-pledge-one-pager.pdf',
+    'osspledge.pdf': '/open-source-pledge-one-pager.pdf',
   },
 
   integrations: [
