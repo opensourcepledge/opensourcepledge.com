68c1908bb0f94fdcbb6680925733e8ad2328beae
Vlad-Stefan Harbuz <vlad@vlad.website>
2026-04-26 20:31:31 +0100

    add testimonial list

--- src/util.ts
+++ src/util.ts
@@ -24,3 +24,17 @@ export function slugify(str: string) {
     .replace(/\s+/g, '-')
     .replace(/-+/g, '-');
 }
+
+export function debounceLeading(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, timeout);
+  };
+}
+
